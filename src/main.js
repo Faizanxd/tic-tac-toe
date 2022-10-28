f5951745cbd2dbbad87a7a2c0cc356ad673c4e72
Faizan <fs8167@gmail.com>
2022-10-28 18:58:56 +0530

    fixing styles

--- src/main.js
+++ src/main.js
@@ -6,33 +6,40 @@ const editOverlay = document.getElementById("overlay");
 const cancelButton = document.getElementById("overlay-cancel");
 const startButton = document.getElementById("start-game");
 const gameOverlay = document.getElementById("active-game");
+const backDrop = document.getElementById("backdrop");
 
 const handleClick1 = editButton1.addEventListener("click", ()=>{
     if(editOverlay.classList.contains("hidden")){
         editOverlay.classList.remove("hidden")
+        backDrop.classList.add("bd")
     }
-    else
+    else 
     editOverlay.classList.add("hidden")
+   backDrop.classList.remove("bd")
 
 });
 
 const handleClick2 = editButton2.addEventListener("click", ()=>{
     if(editOverlay.classList.contains("hidden")){
         editOverlay.classList.remove("hidden")
+        backDrop.classList.add("bd")
     }
     else
     editOverlay.classList.add("hidden")
+    backDrop.classList.remove("bd")
 
 });
 
-// const handleClick3 = cancelButton.addEventListener("click", ()=>{
-//     if(editOverlay.classList.contains("hidden")){
-//         editOverlay.classList.remove("hidden")
-//     }
-// });
+const handleClick3 = cancelButton.addEventListener("click", ()=>{
+    if(editOverlay.classList.contains("hidden")){
+        editOverlay.classList.remove("hidden")
+    }
+});
 
 const handleClick4 = startButton.addEventListener("click", ()=>{
     if(gameOverlay.classList.contains("hidden")){
         gameOverlay.classList.remove("hidden")
     }
+    else
+    gameOverlay.classList.add("hidden")
 });
